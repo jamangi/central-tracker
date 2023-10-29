5724976f0b880a8c21e39283003190c5d7de8b6b
raspberry <jamangi.jmg@gmail.com>
2023-10-29 05:21:35 -0500

    Add consistency pdfs for gardevoir and rhodon

--- earnings/data_gardevoir.py
+++ earnings/data_gardevoir.py
@@ -1,8 +1,7 @@
 import util
 
 GARDEVOIR_DATA = [
-    {"date": "2023-10-21", "gains": 3.01, "time_minutes": 30, "name": "Gardevoir"},
-    {"date": "2023-10-23", "gains": 2.80, "time_minutes": 30, "name": "Gardevoir"},
+    {"date": "2023-10-29", "gains": 3.01, "time_minutes": 60, "name": "Gardevoir"},
 ]
 
 GARDEVOIR_CONSISTENCY_TUPLES = [(datum['time_minutes'], util.to_date(datum['date'])) for datum in GARDEVOIR_DATA]
--- earnings/data_rhodon.py
+++ earnings/data_rhodon.py
@@ -1,8 +1,7 @@
 import util
 
 RHODON_DATA = [
-    {"date": "2023-10-21", "gains": 3.01, "time_minutes": 30, "name": "Rhodon"},
-    {"date": "2023-10-23", "gains": 2.80, "time_minutes": 30, "name": "Rhodon"},
+    {"date": "2023-10-29", "gains": 3.01, "time_minutes": 60, "name": "Rhodon"},
 ]
 
 RHODON_CONSISTENCY_TUPLES = [(datum['time_minutes'], util.to_date(datum['date'])) for datum in RHODON_DATA]
